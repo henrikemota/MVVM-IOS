3e226f5b60de9b59a598b24b6cf296b4c3a33447
Henrique Mota <henrique.mota@topinformation.com.br>
2019-02-14 21:35:13 -0200

    Initial Commit

---
+++ TesteEasyTests/TesteEasyTests.swift
@@ -0,0 +1,34 @@
+//
+//  TesteEasyTests.swift
+//  TesteEasyTests
+//
+//  Created by José Henrique Mota de Oliveira on 14/02/2019.
+//  Copyright © 2019 HK Mobi. All rights reserved.
+//
+
+import XCTest
+@testable import TesteEasy
+
+class TesteEasyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
